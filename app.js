0d766b4e94861508c5cca84bb00b2afc3c3021c3
khayes <khayes@hothouseinc.com>
2020-11-15 10:36:30 -0500

    handle routes

--- app.js
+++ app.js
@@ -4,8 +4,15 @@ const express = require('express');
 const app = express();
 
 // app.use allows us to add middleware (will be excecuted for every incoming request)
-app.use((req, res, next) => {
+app.use('/add-product',(req, res, next) => {
     console.log('Im in middleware');
+    res.send('<h1>Add Product Page</h1>');
+    // next(); allows express to move on to next middleware in line
+});
+
+app.use('/',(req, res, next) => {
+    console.log('Im in another middleware');
+    res.send('<h1>Hello from Express</h1>');
     // next(); allows express to move on to next middleware in line
 });
 
