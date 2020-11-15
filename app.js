24a5d2dd29448b214800f6554d8e86dcac350483
khayes <khayes@hothouseinc.com>
2020-11-15 10:15:57 -0500

    add middleware

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const express = require('express');
 
 const app = express();
 
+// app.use allows us to add middleware (will be excecuted for every incoming request)
+app.use((req, res, next) => {
+    console.log('Im in middleware');
+    // next(); allows express to move on to next middleware in line
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
