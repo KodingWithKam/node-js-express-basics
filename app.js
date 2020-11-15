7ffaee38f138a747ff121748c29f35b144e2e8f0
khayes <khayes@hothouseinc.com>
2020-11-15 11:14:17 -0500

    parsing requests

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+// Body parser
+app.use(bodyParser.urlencoded({extended: false}));
+
 // app.use allows us to add middleware (will be excecuted for every incoming request)
 app.use('/add-product',(req, res, next) => {
-    console.log('Im in middleware');
-    res.send('<h1>Add Product Page</h1>');
+    //console.log('Im in middleware');
+    res.send(`
+        <html lang="en">
+            <title>Add Product</title>
+            <body>
+                <h1>Add Product Page</h1>
+                <form action="/product" method="post"><input type="text" name="title"><button type="submit">Add Product</button></form>
+            </body>
+        </html>`);
     // next(); allows express to move on to next middleware in line
 });
 
+app.use('/product',(req, res, next) => {
+    // Print request body from form
+    console.log(req.body);
+    res.redirect('/');
+});
+
 app.use('/',(req, res, next) => {
-    console.log('Im in another middleware');
+    //console.log('Im in another middleware');
     res.send('<h1>Hello from Express</h1>');
     // next(); allows express to move on to next middleware in line
 });
